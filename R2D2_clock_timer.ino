f418a9f4dba46796dfe46367210fc83f611837de
jejelinge <134136628+jejelinge@users.noreply.github.com>
2023-08-09 19:11:33 +0200

    V2

--- R2D2_clock_timer.ino
+++ R2D2_clock_timer.ino
@@ -3,10 +3,11 @@
 #include "WiFiManager.h"
 #include "NTPClient.h"
 #include "TM1637Display.h"
+#include "DFRobotDFPlayerMini.h"
 
 //========================USEFUL VARIABLES=============================
 const char *ssid     = "SSID"; 
-const char *password = "WIFI PASSWORD";
+const char *password = "PASSWORD";
 const long utcOffsetInSeconds = 7200; // UTC + 2H / Offset in second
 uint16_t notification_volume= 15;
 int Display_backlight = 3; // Set displays brightness 0 to 7;
@@ -16,8 +17,21 @@ int Display_backlight = 3; // Set displays brightness 0 to 7;
 #define CLK 25
 #define DT 26
 #define SW 27
-#define RED_LED 17
-#define WHITE_LED 16
+#define RED_LED 32
+#define WHITE_LED 33
+
+const byte RXD2 = 16; // Connects to module's RX 
+const byte TXD2 = 17; // Connects to module's TX 
+
+#if (defined(ARDUINO_AVR_UNO) || defined(ESP8266))   // Using a soft serial port
+SoftwareSerial softSerial(/*rx =*/4, /*tx =*/5);
+#define FPSerial softSerial
+#else
+#define FPSerial Serial1
+#endif
+
+DFRobotDFPlayerMini myDFPlayer;
+void printDetail(uint8_t type, int value);
 
 float counter = 0;
 int currentStateCLK;
@@ -30,71 +44,8 @@ int secondes = 0;
 int minutes = 0;
 float inc_red_led = 0;
 
-// forward declare the notify class, just the name
-//
-class Mp3Notify; 
-
-// define a handy type using serial and our notify clas
-// Some arduino boards only have one hardware serial port, so a software serial port is needed instead.
-// comment out the above definitions and use these
-SoftwareSerial secondarySerial(18, 19); // RX, TX
-typedef DFMiniMp3<SoftwareSerial, Mp3Notify> DfMp3;
-DfMp3 dfmp3(secondarySerial);
 
 
-class Mp3Notify
-{
-public:
-  static void PrintlnSourceAction(DfMp3_PlaySources source, const char* action)
-  {
-    if (source & DfMp3_PlaySources_Sd) 
-    {
-        Serial.print("SD Card, ");
-    }
-    if (source & DfMp3_PlaySources_Usb) 
-    {
-        Serial.print("USB Disk, ");
-    }
-    if (source & DfMp3_PlaySources_Flash) 
-    {
-        Serial.print("Flash, ");
-    }
-    Serial.println(action);
-  }
-  static void OnError([[maybe_unused]] DfMp3& mp3, uint16_t errorCode)
-  {
-    // see DfMp3_Error for code meaning
-    Serial.println();
-    Serial.print("Com Error ");
-    Serial.println(errorCode);
-  }
-  static void OnPlayFinished([[maybe_unused]] DfMp3& mp3, [[maybe_unused]] DfMp3_PlaySources source, uint16_t track)
-  {
-    Serial.print("Play finished for #");
-    Serial.println(track);  
-
-    // start next track
-    track += 1;
-    // this example will just start back over with 1 after track 3
-    if (track > 3) 
-    {
-      track = 1;
-    }
-    dfmp3.playMp3FolderTrack(track);  // sd:/mp3/0001.mp3, sd:/mp3/0002.mp3, sd:/mp3/0003.mp3
-  }
-  static void OnPlaySourceOnline([[maybe_unused]] DfMp3& mp3, DfMp3_PlaySources source)
-  {
-    PrintlnSourceAction(source, "online");
-  }
-  static void OnPlaySourceInserted([[maybe_unused]] DfMp3& mp3, DfMp3_PlaySources source)
-  {
-    PrintlnSourceAction(source, "inserted");
-  }
-  static void OnPlaySourceRemoved([[maybe_unused]] DfMp3& mp3, DfMp3_PlaySources source)
-  {
-    PrintlnSourceAction(source, "removed");
-  }
-};
 
 // Define NTP Client to get time
 WiFiUDP ntpUDP;
@@ -103,7 +54,13 @@ TM1637Display red1(21, 22);
 
 
 void setup() {
-  
+
+  #if (defined ESP32)
+  FPSerial.begin(9600, SERIAL_8N1, RXD2, TXD2);
+#else
+  FPSerial.begin(9600);
+#endif
+
   pinMode(CLK, INPUT);
   pinMode(DT, INPUT);
   pinMode(SW, INPUT_PULLUP);
@@ -116,26 +73,22 @@ void setup() {
   red1.setBrightness(Display_backlight);
   Serial.begin(115200);
 
-  dfmp3.begin();
-
-  uint16_t version = dfmp3.getSoftwareVersion();
-  Serial.print("version ");
-  Serial.println(version);
-
-  uint16_t volume = dfmp3.getVolume();
-  Serial.print("volume ");
-  Serial.println(volume);
-  dfmp3.setVolume(notification_volume);
+  Serial.println();
+  Serial.println(F("DFRobot DFPlayer Mini Demo"));
+  Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
   
-  uint16_t count = dfmp3.getTotalTrackCount(DfMp3_PlaySource_Sd);
-  Serial.print("files ");
-  Serial.println(count);
+  if (!myDFPlayer.begin(FPSerial, /*isACK = */true, /*doReset = */true)) {  //Use serial to communicate with mp3.
+    Serial.println(F("Unable to begin:"));
+    Serial.println(F("1.Please recheck the connection!"));
+    Serial.println(F("2.Please insert the SD card!"));
+    while(true){
+      delay(0); // Code to compatible with ESP8266 watch dog.
+    }
+  }
+  Serial.println(F("DFPlayer Mini online."));
   
-  Serial.println("starting...");
-
-  // start the first track playing
-  dfmp3.playMp3FolderTrack(1);  // sd:/mp3/0001.mp3
-
+  myDFPlayer.volume(20);  //Set volume value. From 0 to 30
+  myDFPlayer.play(1);  //Play the first mp3
 
   Serial.println("\n Starting");
 
@@ -156,9 +109,13 @@ void setup() {
 
 void loop() {
 
-  timeClient.update();
+ timeClient.update();
  red1.showNumberDecEx(timeClient.getHours(),0b01000000,true,2,0);
  red1.showNumberDecEx(timeClient.getMinutes(),0b01000000,true,2,2);
+
+  if (myDFPlayer.available()) {
+    printDetail(myDFPlayer.readType(), myDFPlayer.read()); //Print the detail message from DFPlayer to handle different errors and states.
+  }
 
   btnState = digitalRead(SW);
   ledcWrite(0, inc_red_led);
@@ -192,7 +149,7 @@ void Setup_timer() {
 
   digitalWrite(RED_LED, HIGH);
   digitalWrite(WHITE_LED, HIGH);
-  dfmp3.playMp3FolderTrack(2);
+  myDFPlayer.play(2);
   delay(500);
 
   btnState = digitalRead(SW);
@@ -240,7 +197,7 @@ void Setup_timer() {
 
 void Countdown (float timer_counter) {
   
-  dfmp3.playMp3FolderTrack(8);
+  myDFPlayer.play(8);;
   delay(1000);
   btnState = digitalRead(SW);
 
@@ -261,7 +218,7 @@ void Countdown (float timer_counter) {
 
     if (timer_counter <= 0) {
 
-      dfmp3.playMp3FolderTrack(3);
+      myDFPlayer.play(3);
 
       for ( int i = 0 ; i < 9 ; i++) {
         ledcWrite(0, 255);
@@ -274,7 +231,7 @@ void Countdown (float timer_counter) {
         waitMilliseconds(random(10, 150));
       }
 
-      dfmp3.playMp3FolderTrack(5);
+      myDFPlayer.play(5);
       
       for ( int i = 0 ; i < 9 ; i++) {
         ledcWrite(0, 255);
@@ -287,7 +244,7 @@ void Countdown (float timer_counter) {
         waitMilliseconds(random(10, 150));
       }
 
-      dfmp3.playMp3FolderTrack(7);
+      myDFPlayer.play(7);
 
       for ( int i = 0 ; i < 9 ; i++) {
         ledcWrite(0, 255);
@@ -317,7 +274,68 @@ void waitMilliseconds(uint16_t msWait)
   {
     // calling mp3.loop() periodically allows for notifications
     // to be handled without interrupts
-    dfmp3.loop();
     delay(1);
   }
+}
+
+void printDetail(uint8_t type, int value){
+  switch (type) {
+    case TimeOut:
+      Serial.println(F("Time Out!"));
+      break;
+    case WrongStack:
+      Serial.println(F("Stack Wrong!"));
+      break;
+    case DFPlayerCardInserted:
+      Serial.println(F("Card Inserted!"));
+      break;
+    case DFPlayerCardRemoved:
+      Serial.println(F("Card Removed!"));
+      break;
+    case DFPlayerCardOnline:
+      Serial.println(F("Card Online!"));
+      break;
+    case DFPlayerUSBInserted:
+      Serial.println("USB Inserted!");
+      break;
+    case DFPlayerUSBRemoved:
+      Serial.println("USB Removed!");
+      break;
+    case DFPlayerPlayFinished:
+      Serial.print(F("Number:"));
+      Serial.print(value);
+      Serial.println(F(" Play Finished!"));
+      break;
+    case DFPlayerError:
+      Serial.print(F("DFPlayerError:"));
+      switch (value) {
+        case Busy:
+          Serial.println(F("Card not found"));
+          break;
+        case Sleeping:
+          Serial.println(F("Sleeping"));
+          break;
+        case SerialWrongStack:
+          Serial.println(F("Get Wrong Stack"));
+          break;
+        case CheckSumNotMatch:
+          Serial.println(F("Check Sum Not Match"));
+          break;
+        case FileIndexOut:
+          Serial.println(F("File Index Out of Bound"));
+          break;
+        case FileMismatch:
+          Serial.println(F("Cannot Find File"));
+          break;
+        case Advertise:
+          Serial.println(F("In Advertise"));
+          break;
+        default:
+          break;
+      }
+      break;
+    default:
+      break;
+  }
+  
 }
